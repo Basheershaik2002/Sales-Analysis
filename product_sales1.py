6c07142a38720e9918d19515b97384a41bba6296
Basheer Shaik <basheershaik7013@gmail.com>
2025-08-13 11:45:51 +0530

    product_sales1.py

--- product_sales1.py
+++ product_sales1.py
@@ -10,7 +10,7 @@ st.title("📊 Product Sales Analysis Dashboard")
 
 # --- Load Excel (hardcoded path from your notebook) ---
 file_path = r"C:\Users\SHAIK BASHEER\Downloads\sales_data_store1.xlsx"
-df = pd.read_excel(file_path)
+df = pd.read_excel(r"C:\Users\SHAIK BASHEER\Downloads\sales_data_store1.xlsx")
 
 # --- Data Cleaning ---
 df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
@@ -125,3 +125,4 @@ fig_anim = px.bar(
 )
 fig_anim.update_layout(yaxis={'categoryorder':'total ascending'})
 st.plotly_chart(fig_anim, use_container_width=True)
+
